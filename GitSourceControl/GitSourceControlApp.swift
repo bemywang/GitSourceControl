525c09bcb21e3b46583d2acbd3cd2f307922ec3e
MinYu Wang <mew.t.minyu@gmail.com>
2024-06-06 16:12:33 +0100

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by Minyu Wang on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
