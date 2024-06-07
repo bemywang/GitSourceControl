afed28d40481b3789996c0254a913bffc4bcb917
MinYu Wang <mew.t.minyu@gmail.com>
2024-06-07 15:19:48 +0100

    My first commit

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            Text("LOVE LONDON")
         }
         .padding()
     }
