0af40a2acbd67cc1e8a503d7150b418f8e58eaae
MinYu Wang <mew.t.minyu@gmail.com>
2024-06-10 15:02:47 +0100

    [Features] button on second view

--- GitSourceControl/SecondScreenView.swift
+++ GitSourceControl/SecondScreenView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct SecondScreenView: View {
+    @State private var showAlert = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("Show Alert") {
+                showAlert = true
+            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Test"),
+                message: Text("Function Testing"),
+                dismissButton: .default(Text("OK"))
+            )
+        }
     }
 }
 
