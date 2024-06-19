d5fefaa4c91d7ac381ffe8b68c7c00a419c574e6
MinYu Wang <mew.t.minyu@gmail.com>
2024-06-19 11:14:58 +0100

    [Feature] add notification color

--- GitSourceControl/NotificationView.swift
+++ GitSourceControl/NotificationView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct NotificationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "bell.fill")
+                .foregroundColor(.red)
+            Text("Notification")
+        }
+        .font(.largeTitle)
     }
 }
 
