2bfebd2001987e61d0e90c3f7c0ef56525e27ff4
MinYu Wang <mew.t.minyu@gmail.com>
2024-06-19 11:12:31 +0100

    [Feature] Create a new file for Notification	view

---
+++ GitSourceControl/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  GitSourceControl
+//
+//  Created by Minyu Wang on 19/06/2024.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
